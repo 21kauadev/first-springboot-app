b03282a92467f25cf221e7db17293b60838ba35a
21kauadev <kauantnetsoares@gmail.com>
2025-03-03 10:47:49 -0300

    add files

---
+++ src/main/java/com/kaua/first_string_app/FirstStringAppApplication.java
@@ -0,0 +1,13 @@
+package com.kaua.first_string_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstStringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstStringAppApplication.class, args);
+	}
+
+}
